d5a5efbc0777e4e72eeebea426430f4823aa2351
LuBrian <blu1@ualberta.ca>
2017-02-23 18:20:24 -0700

    working on signin signup pages and major classes

--- app/src/main/java/com/assign1/brianlu/mooditfromorbit/MoodMainActivity.java
+++ app/src/main/java/com/assign1/brianlu/mooditfromorbit/MoodMainActivity.java
@@ -2,21 +2,107 @@ package com.assign1.brianlu.mooditfromorbit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
-import java.lang.reflect.Array;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MoodMainActivity extends AppCompatActivity {
-    private static final String FILENAME = "mooduser.sav";
-//    private EditTex
-
+    private static final String FILENAME = "file.sav";
     private ArrayList<User> users = new ArrayList<User>();
-    private ArrayAdapter<User> adapter;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button signInButton = (Button) findViewById(R.id.signIn);
+        Button signUpButton = (Button) findViewById(R.id.signUp);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("click sign in", "click signed in button");
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("click sign up", "clicked sign Up");
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        loadFromFile();
+//        adapter = new ArrayAdapter<User>(this,
+//                R.layout.list_item, user);
+//        oldTweetsList.setAdapter(adapter);
+    }
+
+
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+            // Took from https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html Jan-21-2016
+            Type listType = new TypeToken<ArrayList<User>>() {}.getType();
+            users = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            users = new ArrayList<User>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    0);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+            Gson gson = new Gson();
+            gson.toJson(users, out);
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
     }
 }
