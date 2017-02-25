5abd3adcf63998e4132ab0aa111021cbe5c7fcb4
Greg <gdbaker@ualberta.ca>
2017-02-24 18:20:57 -0700

    Added Profile class

---
+++ app/src/main/java/com/assign1/brianlu/mooditfromorbit/Profile.java
@@ -0,0 +1,16 @@
+package com.assign1.brianlu.mooditfromorbit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gregory on 2017-02-24.
+ */
+
+public class Profile {
+    private String username;
+    private ArrayList<Mood> moodHistory;
+
+    public Profile(String username){
+        this.username = username;
+    }
+}
