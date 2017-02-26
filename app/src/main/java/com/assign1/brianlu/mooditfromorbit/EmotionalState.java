612dd1b1eb0358c62a591a6145a1cb8e0fe596b2
cqtran <Cameron@Camerons-MBP.hitronhub.home>
2017-02-26 12:56:35 -0800

    added emotional state class

---
+++ app/src/main/java/com/assign1/brianlu/mooditfromorbit/EmotionalState.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) $2017 CMPUT 301 University of Alberta. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.assign1.brianlu.mooditfromorbit;
+
+/**
+ * Created by Cameron on 2017-02-26.
+ */
+
+public class EmotionalState {
+
+    public String moodAnger(){ return new String("Anger");}
+
+    public String moodConfusion(){ return new String("Confusion");}
+
+    public String moodDisgust(){ return new String("Disgust");}
+
+    public String moodFear(){ return new String("Fear");}
+
+    public String moodHappiness(){ return new String("Happiness");}
+
+    public String moodSadness(){ return new String("Sadness");}
+
+    public String moodShame(){ return new String("Shame");}
+
+    public String moodSuprise(){ return new String("Suprise");}
+
+}
