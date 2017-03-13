948c55cf9c223f0813e6387d0807917875eb2af0
Greg <gdbaker@ualberta.ca>
2017-03-13 03:12:56 -0600

    added ui test for new mood

--- app/src/androidTest/java/com/assign1/brianlu/mooditfromorbit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/assign1/brianlu/mooditfromorbit/ExampleInstrumentedTest.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,16 +20,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 //@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2 {
+public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2<MoodMainActivity> {
+    private Solo solo;
 
     public ExampleInstrumentedTest() {
         super(com.assign1.brianlu.mooditfromorbit.MoodMainActivity.class);
+    }
+
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
     }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -34,5 +43,33 @@ public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2 {
 
         assertEquals("com.assign1.brianlu.mooditfromorbit", appContext.getPackageName());
     }
+
+    /**
+     * tests if new mood was added
+     */
+    public void testNewMood(){
+        solo.assertCurrentActivity("Wrong activity", MoodMainActivity.class);
+        solo.clickOnButton("Sign In");
+        solo.assertCurrentActivity("Wrong activity", SignInActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.signInInput), "Greg");
+        solo.clickOnButton("Log In");
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+        solo.clickOnView(solo.getView(R.id.action_add_mood));
+        solo.assertCurrentActivity("Wrong activity", AddMood.class);
+        String text = "abcde1";
+        solo.enterText((EditText) solo.getView(R.id.comment), text);
+        solo.clickOnButton("Done");
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+
+        MainController mc = MainApplication.getMainController();
+
+        Mood mood = mc.getMe().getMoods().getMood(0);
+
+        assertEquals(text,mood.getMessage());
+
+        solo.goBack();
+    }
+
+
 
 }
