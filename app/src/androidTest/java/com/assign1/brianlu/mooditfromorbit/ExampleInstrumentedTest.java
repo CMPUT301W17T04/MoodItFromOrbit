b2d20146aceb84b24d2d4851f54fd3d467f4131e
cqtran <Cameron@Camerons-MBP.ed.shawcable.net>
2017-03-13 15:00:47 -0600

    added spinner and camera tests
    added in ExampleInstrumentedTest

--- app/src/androidTest/java/com/assign1/brianlu/mooditfromorbit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/assign1/brianlu/mooditfromorbit/ExampleInstrumentedTest.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
@@ -69,7 +72,80 @@ public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2<Mo
 
         solo.goBack();
     }
+//tests if the spinners are working
+    public void testSpinners(){
+        solo.assertCurrentActivity("Wrong activity", MoodMainActivity.class);
+        solo.clickOnButton("Sign In");
+        solo.assertCurrentActivity("Wrong activity", SignInActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.signInInput), "Cameron");
+        solo.clickOnButton("Log In");
+
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+        solo.clickOnView(solo.getView(R.id.action_add_mood));
+        solo.assertCurrentActivity("Wrong activity", AddMood.class);
 
 
 
+        // 0 is the first spinner in the layout
+        View view1 = solo.getView(Spinner.class, 0);
+        solo.clickOnView(view1);
+        solo.scrollToTop(); // I put this in here so that it always keeps the list at start
+        // select the 3rd item in the spinner
+        solo.clickOnView(solo.getView(TextView.class, 3));
+
+        String text = "I am disgusted";
+
+        View view2 = solo.getView(Spinner.class, 1);
+        //solo.scrollToTop();
+        solo.clickOnView(view2);
+        solo.clickOnView(solo.getView(TextView.class, 3));
+
+
+        solo.enterText((EditText) solo.getView(R.id.comment), text);
+
+        solo.clickOnButton("Done");
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+        solo.clickOnMenuItem("Profile");
+
+
+        solo.goBack();
+
+        MainController mc = MainApplication.getMainController();
+
+        Mood mood = mc.getMe().getMoods().getMood(0);
+
+        assertEquals(text,mood.getMessage());
+
+        solo.goBack();
+
+    }
+
+
+    //Test for if camera works
+    public void testCamera(){
+        solo.assertCurrentActivity("Wrong activity", MoodMainActivity.class);
+        solo.clickOnButton("Sign In");
+        solo.assertCurrentActivity("Wrong activity", SignInActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.signInInput), "Cameron");
+        solo.clickOnButton("Log In");
+
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+        solo.clickOnView(solo.getView(R.id.action_add_mood));
+        solo.assertCurrentActivity("Wrong activity", AddMood.class);
+
+        // tODO
+        solo.clickOnButton("Take Photo");
+        // This is where you capture a picture manually and press the checkmark
+
+
+
+        solo.clickOnButton("Done");
+        solo.assertCurrentActivity("Wrong activity", DashBoard.class);
+        solo.clickOnMenuItem("Profile");
+
+
+        solo.goBack();
+        solo.goBack();
+
+    }
 }
