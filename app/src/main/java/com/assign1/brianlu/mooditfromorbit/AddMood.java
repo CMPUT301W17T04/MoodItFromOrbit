65215c56ecbc3dd66b66c7ed6344bfacfab51f94
onlookers <jcao5@ualbeta.ca>
2017-04-01 18:21:57 -0600

    comment check

--- app/src/main/java/com/assign1/brianlu/mooditfromorbit/AddMood.java
+++ app/src/main/java/com/assign1/brianlu/mooditfromorbit/AddMood.java
@@ -94,6 +94,9 @@ public class AddMood extends AppCompatActivity implements MView<MainModel> {
                 // check if the input comment is larger than 3 words
                 Boolean Check = WordsCheck(commentstring);
 
+                if (!Check){
+                    Log.i("length","The comment is more than 3 words!");
+                }
 
 
                 MainController mc = MainApplication.getMainController();
@@ -179,17 +182,22 @@ public class AddMood extends AppCompatActivity implements MView<MainModel> {
         }
     }
 
-// check if the input word is more than 3 words,if so, return false, if less than 3 words, return ture
+// check if the input word is more than 3 words or more than 20 characters
+// ,if so, return false, if less than 3 words, return ture
 
     public boolean WordsCheck(String comment){
         Boolean Lengthcheck = true;
         String userinput = comment.trim();
         if (userinput == null) {
             Lengthcheck = true;
-        }else if (userinput.split("\\s+").length <3 ) {
-            Lengthcheck = true;
-        }else {
-                Lengthcheck = false;
+        }else if (userinput.split("\\s+").length >3 ) {
+            Lengthcheck = false;
+
+        }else if (userinput.length()>20){
+            Lengthcheck = false;
+        }
+        else {
+                Lengthcheck = true;
             }
         return Lengthcheck;
 
